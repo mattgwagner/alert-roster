30d6f2bebf53631eaed227bb09489e317d77dd18
Matt Wagner <mattgwagner@gmail.com>
2014-01-08 17:36:01 -0600

    Add connection string

--- alert-roster.web/Models/AlertRosterDbContext.cs
+++ alert-roster.web/Models/AlertRosterDbContext.cs
@@ -8,6 +8,12 @@ namespace alert_roster.web.Models
 {
     public class AlertRosterDbContext : DbContext
     {
+        public AlertRosterDbContext()
+            : base("MainDb")
+        {
+
+        }
+
         public DbSet<Message> Messages { get; set; }
     }
 }
