86bca34fe0882694cd0086187473a7ccca2290ed
Matt Wagner <mattgwagner@gmail.com>
2014-01-08 17:21:01 -0600

    Enable package restore

---
+++ alert-roster.web/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace alert_roster.web.Models
+{
+    public class Message
+    {
+        public Guid ID { get; set; }
+
+        public DateTime PostedDate { get; set; }
+
+        public String Content { get; set; }
+    }
+}
