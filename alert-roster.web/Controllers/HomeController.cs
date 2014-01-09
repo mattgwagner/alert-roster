ab15fdd68bfcbb5c878e87083156a3d61b171351
Matt Wagner <mattgwagner@gmail.com>
2014-01-09 17:13:29 -0600

    Fixed authentication

--- alert-roster.web/Controllers/HomeController.cs
+++ alert-roster.web/Controllers/HomeController.cs
@@ -1,14 +1,20 @@
 ﻿using alert_roster.web.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace alert_roster.web.Controllers
 {
     public class HomeController : Controller
     {
+        private String ReadOnlyPassord { get { return ConfigurationManager.AppSettings["Password.ReadOnly"]; } }
+
+        private String ReadWritePassword { get { return ConfigurationManager.AppSettings["Password.ReadWrite"]; } }
+
         public ActionResult Index()
         {
             using (var db = new AlertRosterDbContext())
@@ -17,11 +23,6 @@ namespace alert_roster.web.Controllers
 
                 return View(messages);
             }
-        }
-
-        public ActionResult About()
-        {
-            return View();
         }
 
         [AllowAnonymous]
@@ -33,7 +34,19 @@ namespace alert_roster.web.Controllers
         [AllowAnonymous, HttpPost, ValidateAntiForgeryToken]
         public ActionResult Login(String password)
         {
-            // TODO Check password
+            if (ReadOnlyPassord == password)
+            {
+                FormsAuthentication.SetAuthCookie("ReadOnly", true);
+            }
+            else if (ReadWritePassword == password)
+            {
+                FormsAuthentication.SetAuthCookie("ReadWrite", true);
+            }
+            else
+            {
+                // Miss, send back to login for now
+                return RedirectToAction("Login");
+            }
 
             return RedirectToAction("Index");
         }
