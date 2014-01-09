32596f490d4a36c5f4952c8f618266eb78f53e1e
Matt Wagner <mattgwagner@gmail.com>
2014-01-08 18:59:00 -0600

    Consolidate filter and route config

--- alert-roster.web/Global.asax.cs
+++ alert-roster.web/Global.asax.cs
@@ -13,9 +13,23 @@ namespace alert_roster.web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            RegisterRoutes(RouteTable.Routes);
+
+            GlobalFilters.Filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
