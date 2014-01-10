60cd2d55b661f85f929d9d75a9b22c99ecda4bf9
Matt Wagner <mattgwagner@gmail.com>
2014-01-10 13:32:04 -0600

    Finally get display time right, I think

--- alert-roster.web/Models/Message.cs
+++ alert-roster.web/Models/Message.cs
@@ -13,4 +13,14 @@ namespace alert_roster.web.Models
 
         public String Content { get; set; }
     }
+
+    public static class DateTimeExtensions
+    {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static double GetEpochTicks(this DateTime dateTime)
+        {
+            return dateTime.Subtract(Epoch).TotalMilliseconds;
+        }
+    }
 }
