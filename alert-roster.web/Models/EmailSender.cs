93637eea759f5f1a7c548fe86f5a3a925140387a
Matt Wagner <mattgwagner@gmail.com>
2014-01-10 20:37:12 -0600

    Initial email sending code added

---
+++ alert-roster.web/Models/EmailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace alert_roster.web.Models
+{
+    public class EmailSender
+    {
+        public static String SmtpServer = ConfigurationManager.AppSettings["MAILGUN_SMTP_SERVER"];
+
+        public static int SmtpPort = int.Parse(ConfigurationManager.AppSettings["MAILGUN_SMTP_PORT"]);
+
+        public static String SmtpUser = ConfigurationManager.AppSettings["MAILGUN_SMTP_LOGIN"];
+
+        public static String SmtpPassword = ConfigurationManager.AppSettings["MAILGUN_SMTP_PASSWORD"];
+
+        public static Boolean EnableSsl = true;
+
+        public void Send(String content, IEnumerable<String> Recipients)
+        {
+            using (var smtp = new SmtpClient { Host = SmtpServer, Port = SmtpPort, EnableSsl = EnableSsl, Credentials = new NetworkCredential { UserName = SmtpUser, Password = SmtpPassword } })
+            using (var message = new MailMessage { IsBodyHtml = false })
+            {
+                message.From = new MailAddress("");
+
+                message.To.Add(new MailAddress(""));
+
+                foreach (var recipient in Recipients)
+                {
+                    message.Bcc.Add(new MailAddress(recipient));
+                }
+
+                message.Subject = "";
+
+                message.Body = "";
+
+                smtp.Send(message);
+            }
+    }
+}
