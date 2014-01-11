ba0b4fc2d0d61fc8105650c9d5d63bf08d815987
Matt Wagner <mattgwagner@gmail.com>
2014-01-10 20:41:36 -0600

    Use from address config val

--- alert-roster.web/Models/EmailSender.cs
+++ alert-roster.web/Models/EmailSender.cs
@@ -29,9 +29,8 @@ namespace alert_roster.web.Models
             using (var smtp = new SmtpClient { Host = SmtpServer, Port = SmtpPort, EnableSsl = EnableSsl, Credentials = new NetworkCredential { UserName = SmtpUser, Password = SmtpPassword } })
             using (var message = new MailMessage { IsBodyHtml = IsBodyHtml })
             {
-                message.From = new MailAddress("");
-
-                message.To.Add(new MailAddress(""));
+                message.From = new MailAddress(FromAddress);
+                message.To.Add(new MailAddress(FromAddress));
 
                 foreach (var recipient in Recipients)
                 {
