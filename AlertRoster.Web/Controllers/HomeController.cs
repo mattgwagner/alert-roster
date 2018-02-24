c40f2d54d82298d11acf41dd508906e6b127b9b8
Matt Wagner <mattgwagner@gmail.com>
2018-02-24 14:19:01 -0500

    Stubbing out more API endpoints

--- AlertRoster.Web/Controllers/HomeController.cs
+++ AlertRoster.Web/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
 ﻿using AlertRoster.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AlertRoster.Web.Controllers
 {
     public class HomeController : Controller
     {
+        // List/Add/Remove Members
+
         private readonly Database db;
 
         public HomeController(Database db)
@@ -24,12 +29,105 @@ namespace AlertRoster.Web.Controllers
             };
         }
 
-        // Show Groups User Can See
+        [HttpGet("/api/groups/{id}")]
+        public dynamic GetGroups(int? id)
+        {
+            return
+                db
+                .Groups
+                .Where(group => !id.HasValue || group.Id == id)
+                .Select(group => new
+                {
+                    group.Id,
+                    group.DisplayName,
+                    group.PhoneNumber,
+                    members =
+                        group
+                        .Members
+                        .Select(member => new
+                        {
+                            member.MemberId,
+                            member.Member.DisplayName,
+                            member.Role
+                        })
+                });
+        }
 
-        // View Group Details
+        [HttpPost("/api/groups")]
+        public async Task<IActionResult> CreateGroup(String displayName)
+        {
+            var result = db.Groups.Add(new Group(displayName));
 
-        // Add/Remove Groups
+            await db.SaveChangesAsync();
 
-        // Add/Remove Members
+            // TODO Go get phone #
+
+            return GetGroups(result.Entity.Id);
+        }
+
+        [HttpDelete("/api/groups/{id}")]
+        public async Task<IActionResult> RemoveGroup(int id)
+        {
+            var group =
+                await db
+                .Groups
+                .SingleOrDefaultAsync(_ => _.Id == id);
+
+            // Perhaps we can mark this archived instead?
+            // Maybe reject should be only admins can send?
+
+            // group.Replies = Group.ReplyMode.Reject;
+
+            db.Groups.Remove(group);
+
+            await db.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpGet("/api/groups/{id}/messages")]
+        public async Task<dynamic> GetMessages(int id, DateTime? since)
+        {
+            return
+                await db
+                .Messages
+                .Where(msg => msg.GroupId == id)
+                .Where(msg => !since.HasValue || msg.Timestamp >= since)
+                .Select(msg => new
+                {
+                    msg.Id,
+                    msg.Timestamp,
+                    msg.Status,
+                    msg.SenderId,
+                    msg.DisplayContent
+                })
+                .ToListAsync();
+        }
+
+        [HttpGet("/api/groups/{id}/members")]
+        public async Task<dynamic> GetMembers(int id)
+        {
+            return
+                await db
+                .MemberGroups
+                .Where(group => group.GroupId == id)
+                .Select(group => group.Member)
+                .Select(member => new
+                {
+                    member.Id,
+                    member.DisplayName
+                })
+                .ToListAsync();
+        }
+
+        [HttpPost("/api/groups/{id}/members")]
+        public async Task<IActionResult> AddMember(int id, [FromBody]String phone, [FromBody]String displayName)
+        {
+            // TODO Find or create user, add them to the group
+
+            await db.SaveChangesAsync();
+
+            return Accepted();
+        }
     }
 }
