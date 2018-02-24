2bd0b91e16e0e7e02d41cc0494f6e8cc669f8a6c
Matt Wagner <mattgwagner@gmail.com>
2018-02-24 13:30:18 -0500

    Add reply mode and a shortcut for getting admins

--- AlertRoster.Web/Models/Group.cs
+++ AlertRoster.Web/Models/Group.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace AlertRoster.Web.Models
 {
@@ -17,11 +18,12 @@ namespace AlertRoster.Web.Models
 
         public virtual ICollection<MemberGroup> Members { get; private set; }
 
+        public virtual IEnumerable<Member> Admins => Members.Where(_ => _.Role == MemberGroup.GroupRole.Administrator).Select(_ => _.Member);
+
         public virtual ICollection<Message> Messages { get; private set; }
 
-        // TODO Who are admins?
-
-        // TODO Reply-mode : Reply-All, Reply-to-Admin, Reject?
+        [Required]
+        public ReplyMode Replies { get; set; } = ReplyMode.ReplyAll;
 
         private Group()
         {
@@ -33,6 +35,15 @@ namespace AlertRoster.Web.Models
             this.DisplayName = displayName;
             this.Members = new List<MemberGroup>();
             this.Messages = new List<Message>();
+        }
+
+        public enum ReplyMode : byte
+        {
+            ReplyAll = 0,
+
+            ReplyToAdmins = 1,
+
+            Reject = 2
         }
     }
 }
